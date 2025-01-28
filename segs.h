6ef39e063a6472afd53ee3b28555a27bac3086e3
Mikhail Kirechko <kirechko@gmail.com>
2025-01-28 21:40:20 +0300

    Create segs.h

---
+++ segs.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "esphome/core/component.h"
+#include "esphome/components/neopixelbus/neopixelbus_light.h"
+
+namespace esphome {
+namespace segs {
+
+class Segs : public neopixelbus::NeoPixelBusLightOutputBase {
+};
+
+}  // namespace segs
+}  // namespace esphome
