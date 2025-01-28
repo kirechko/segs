d0dbddaddd4c21e66d520db686cb6a56ff2f649c
Mikhail Kirechko <kirechko@gmail.com>
2025-01-28 21:37:02 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,23 +1,25 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import esp32_ble_tracker
+from esphome.components import light
+from esphome.components import neopixelbus
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ["esp32_ble_tracker"]
-CODEOWNERS = ["@jeromelaban"]
+DEPENDENCIES = ["neopixelbus"]
+CODEOWNERS = ["@kirechko"]
 
-airthings_ble_ns = cg.esphome_ns.namespace("airthings_ble")
-AirthingsListener = airthings_ble_ns.class_(
-    "AirthingsListener", esp32_ble_tracker.ESPBTDeviceListener
+segs_ns = cg.esphome_ns.namespace("segs")
+Segs = segs_ns.class_(
+    "Segs", neopixelbus.NeoPixelBusLightOutputBase
 )
 
 CONFIG_SCHEMA = cv.Schema(
     {
-        cv.GenerateID(): cv.declare_id(AirthingsListener),
+        cv.GenerateID(): cv.declare_id(Segs),
     }
-).extend(esp32_ble_tracker.ESP_BLE_DEVICE_SCHEMA)
+).extend(neopixelbus.ESP_BLE_DEVICE_SCHEMA)
 
 
 def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    yield esp32_ble_tracker.register_ble_device(var, config)
+    await light.register_light(var, config)
+    await cg.register_component(var, config)
